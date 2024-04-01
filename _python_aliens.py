346cbe9a594d6d5036b08a679e12b7880af65b70
Jonathan Munoz <madzumo@gmail.com>
2024-03-31 22:20:56 -0400

    moving aliens

--- _python_aliens.py
+++ _python_aliens.py
@@ -35,6 +35,7 @@ class AlienInvasion:
             self.ship.update()
             self.bullets.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -91,9 +92,45 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """create the fleet of aliens"""
+        # create an alien and keep adding aliens until there is no room left
+        # spacing between aliens is one alien width
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
 
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            # finished a row; reset x value and increment y value.
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        """create an alien and place it in the row"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+
+    def _update_aliens(self):
+        """update the positions of all aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """drop the entire fleet and change the fleet direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- alien.py
+++ alien.py
@@ -8,8 +8,9 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
-        # load alient image
+        # load alien image
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
@@ -19,3 +20,14 @@ class Alien(Sprite):
 
         # store the aliens exact horizontal position
         self.x = float(self.rect.x)
+
+    def update(self):
+        """move the alien right or left"""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+
+    def check_edges(self):
+        """return True if alien is at edge of screen"""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
